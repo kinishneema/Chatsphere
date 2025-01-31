f396ee156ac00a55350c9945da21adfc0fd70911
PostgreSQL <postgres@MacBook-Air.local>
2025-01-31 17:00:09 +0530

    1st part completed - backend

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,11 @@
+import express from "express"
+import protectRoute from '../middleware/protectRoute.js';
+import { getUsersforSidebar } from '../controllers/user.controller.js';
+
+
+
+const router = express.Router();
+
+router.get("/",protectRoute, getUsersforSidebar);
+
+export default router;
